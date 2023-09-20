4e2dc95bced42eb6cb03bf8472bf32e19b24101d
정진관 <dingwan0331@gmail.com>
2023-09-20 20:54:49 +0900

    chore: spring initial setting

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+plugins {
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version Dependency.springBootVersion
+    kotlin("jvm") version Dependency.kotlinVersion
+    kotlin("plugin.spring") version Dependency.kotlinVersion
+    kotlin("kapt") version Dependency.kotlinVersion
+}
+
+java.sourceCompatibility = JavaVersion.toVersion(Dependency.targetJvmVersion)
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${Dependency.springCloudVersion}")
+    }
+}
+
+apply(plugin = "kotlin-kapt")
+dependencies {
+    // MVC
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = Dependency.targetJvmVersion
+        }
+    }
+}
---
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -0,0 +1,6 @@
+object Dependency {
+    const val kotlinVersion = "1.7.22"
+    const val springBootVersion = "3.0.4"
+    const val springCloudVersion = "2022.0.1"
+    const val targetJvmVersion = "17"
+}
