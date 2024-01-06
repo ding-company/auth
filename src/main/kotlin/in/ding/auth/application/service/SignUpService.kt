640155e09c25590d4aa72df5a85df7c87f7feacb
정진관 <dingwan0331@gmail.com>
2024-01-06 18:28:24 +0900

    feat: service레이어에서 도메인 register함수 호출

--- src/main/kotlin/in/ding/auth/application/service/SignUpService.kt
+++ src/main/kotlin/in/ding/auth/application/service/SignUpService.kt
@@ -2,10 +2,15 @@ package `in`.ding.auth.application.service
 
 import `in`.ding.auth.application.port.`in`.SignUpCommand
 import `in`.ding.auth.application.port.`in`.SignUpUseCase
+import `in`.ding.auth.domain.User
 
-class SignUpService:SignUpUseCase {
+class SignUpService : SignUpUseCase {
     override fun signUp(command: SignUpCommand): Boolean {
-        TODO("Not yet implemented")
+        User.register(
+            mobilePhoneNumber = command.mobilePhoneNumber,
+            name = command.name, nickName = command.nickName, password = command.password
+        )
+        return true
     }
 
 }
