34d936ef656604f3cb5716b01da93012a7852211
정진관 <dingwan0331@gmail.com>
2024-01-06 18:28:05 +0900

    feat: User domain 작성

--- src/main/kotlin/in/ding/auth/domain/User.kt
+++ src/main/kotlin/in/ding/auth/domain/User.kt
@@ -2,9 +2,20 @@ package `in`.ding.auth.domain
 
 import java.util.UUID
 
-class User(
-    id: UUID,
-    mobilePhoneNumber: String,
-    name: String,
-    nickName: String?
-)
+data class User private constructor(
+    var id: UUID,
+    val mobilePhoneNumber: String,
+    val name: String,
+    val nickName: String?,
+    val password: String,
+) {
+    companion object {
+        fun register(mobilePhoneNumber: String, name: String, nickName: String? = null, password: String): User {
+            return User(
+                id = UUID.randomUUID(),
+                mobilePhoneNumber = mobilePhoneNumber,
+                name = name, nickName = nickName, password = password
+            )
+        }
+    }
+}
